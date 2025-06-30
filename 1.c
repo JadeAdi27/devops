3f1a13aeef853d874aebf788c793dd2373b98db1
Adi <115174973+JadeAdi27@users.noreply.github.com>
2025-07-01 01:56:39 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,52 @@
+//Myapp.java
+import java.util.ResourceBundle;
+public class MyApp {
+    public int userLogin(String inUser, String inPwd) {
+        ResourceBundle rb = ResourceBundle.getBundle("config");
+        String username = rb.getString("username");
+        String password = rb.getString("password");
+        if (inUser.equals(username) && inPwd.equals(password)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+--------------------------------------
+// Config.properties
+  
+username=abc
+password=abc@1234
+  ------------------------------------
+  -----------------------------------
+  //MyAppTest.java code
+  
+  package myproject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.myproject.MyApp;
+public class MyAppTest {
+    @Test
+    public void testLogin1() {
+        MyApp myApp = new MyApp();
+        Assert.assertEquals(myApp.userLogin("abc", "abc1234"), 0);
+    }
+    @Test
+    public void testLogin2() {
+        MyApp myApp = new MyApp();
+        Assert.assertEquals(myApp.userLogin("abc", "abc@1234"), 1);
+    }
+}
+-----------------------------------
+  //pop
+  <dependencies>
+  <!-- https://mvnrepository.com/artifact/org.testng/testng -->
+  <dependency>
+    <groupId>org.testng</groupId>
+    <artifactId>testng</artifactId>
+    <version>7.5.1</version>
+    <scope>test</scope>
+  </dependency>
+</dependencies>
+-------------------------------
+  gradle init
